e772e79d24e4edc0ec82f2dc7e5212eaad67abda
Dominik Grenz <Dominik Grenz>
2017-03-27 14:55:59 +0200

    Initial Commit

---
+++ src/main/java/edu/hm/swa/todo/Praktikum1/App.java
@@ -0,0 +1,13 @@
+package edu.hm.swa.todo.Praktikum1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
