09d5b4eb2271e33b518f0029d225006662ca9723
Tobias Kroiss <tobi.kroiss@googlemail.com>
2017-04-03 13:51:36 +0200

    Created SomeClass.java

---
+++ src/main/java/edu/hm/cs/swa/todo/Aufgabe01/SomeClass.java
@@ -0,0 +1,20 @@
+package edu.hm.cs.swa.todo.Aufgabe01;
+
+import java.io.*;
+import java.util.*;
+//import edu.hm.renderer.*;
+
+public class SomeClass {
+
+	@RenderMe
+	private int foo;
+	@RenderMe(with = "edu.hm.renderer.ArrayRenderer")
+	int[] array = { 1, 2, 3, };
+	@RenderMe
+	private Date date = new Date(123456789);
+
+	public SomeClass(int foo) {
+		this.foo = foo;
+	}
+
+}
