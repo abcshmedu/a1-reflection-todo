1378c41f6d18155809e76a6abcdd52563987ac2f
DomixD <dominik-grenz@t-online.de>
2017-04-04 14:35:11 +0200

    initalize render()

--- src/main/java/edu/hm/cs/swa/todo/Aufgabe01/Renderer.java
+++ src/main/java/edu/hm/cs/swa/todo/Aufgabe01/Renderer.java
@@ -1,10 +1,45 @@
 package edu.hm.cs.swa.todo.Aufgabe01;
 
+import java.lang.reflect.Field;
+
 public class Renderer {
 
-	private SomeClass renderer;
-	
-	private Renderer(SomeClass renderer){
-		this.renderer=renderer;
+	private Object obj_render;
+
+	public Renderer(Object obj_render) {
+		this.obj_render = obj_render;
+	}
+
+	public String render() {
+		String result = "";
+		Class<?> cls = obj_render.getClass();
+		result = "Class:" + cls.getName() + "\n";
+		Field[] fields = cls.getDeclaredFields();
+		for (Field f : fields) {
+			RenderMe annotation = f.getAnnotation(RenderMe.class);
+			if (annotation != null) {
+				f.setAccessible(true);
+				result += "Name:" + f.getName() + " ";
+				if (annotation.with().equals("")) {
+					try {
+						result += "(Type " + f.getType().getName() + "): " + f.get(obj_render) + "\n";
+
+					} catch (Exception e) {
+
+					}
+				} else {
+
+				}
+
+			}
+		}
+		return result;
+	}
+
+	public static void main(String... strings) {
+		SomeClass toRender = new SomeClass(5);
+		Renderer renderer = new Renderer(toRender);
+		System.out.println(renderer.render());
+
 	}
 }
