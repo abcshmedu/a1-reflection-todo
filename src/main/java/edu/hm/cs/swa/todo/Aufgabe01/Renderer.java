cc8892d4a7f0d51339de2984f0820d83afdd1a37
DomixD <dominik-grenz@t-online.de>
2017-04-04 19:53:53 +0200

    finished render()

--- src/main/java/edu/hm/cs/swa/todo/Aufgabe01/Renderer.java
+++ src/main/java/edu/hm/cs/swa/todo/Aufgabe01/Renderer.java
@@ -1,7 +1,11 @@
 package edu.hm.cs.swa.todo.Aufgabe01;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
+/**
+ * @author Dominik Grenz & Tobias Kroiss
+ */
 public class Renderer {
 
 	private Object obj_render;
@@ -13,21 +17,29 @@ public class Renderer {
 	public String render() {
 		String result = "";
 		Class<?> cls = obj_render.getClass();
-		result = "Class:" + cls.getName() + "\n";
+		result = "Instance of " + cls.getName() + ":\n";
 		Field[] fields = cls.getDeclaredFields();
 		for (Field f : fields) {
 			RenderMe annotation = f.getAnnotation(RenderMe.class);
 			if (annotation != null) {
 				f.setAccessible(true);
-				result += "Name:" + f.getName() + " ";
+				result += f.getName() + " ";
 				if (annotation.with().equals("")) {
 					try {
 						result += "(Type " + f.getType().getName() + "): " + f.get(obj_render) + "\n";
 
 					} catch (Exception e) {
-
+						e.printStackTrace();
 					}
 				} else {
+					try {
+						Class<?> array = Class.forName(annotation.with());
+						Method method = array.getMethod("render", f.getType());
+						result += method.invoke(array.newInstance(), f.get(obj_render));
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 
 				}
 
