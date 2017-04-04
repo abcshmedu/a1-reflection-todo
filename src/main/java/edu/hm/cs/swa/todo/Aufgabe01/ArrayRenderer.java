dd1976f5ce4fa06efd5cf7bb83ee071034e6e5ca
DomixD <dominik-grenz@t-online.de>
2017-04-04 19:51:20 +0200

    Created ArrayRenderer.java

---
+++ src/main/java/edu/hm/cs/swa/todo/Aufgabe01/ArrayRenderer.java
@@ -0,0 +1,17 @@
+package edu.hm.cs.swa.todo.Aufgabe01;
+
+/**
+ * @author Dominik Grenz & Tobias Kroiss
+ */
+public class ArrayRenderer {
+
+	public String render(int[] array) {
+		String result = "(Type int[]) [";
+		for (int index : array) {
+			result += index + ", ";
+		}
+		result += "]\n";
+		return result;
+	}
+
+}
