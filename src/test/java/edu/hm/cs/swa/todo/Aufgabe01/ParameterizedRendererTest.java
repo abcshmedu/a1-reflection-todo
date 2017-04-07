fb22123a49bffd0d2adbcf441b8a859d69d19aaa
Tobias Kroiss <tobi.kroiss@googlemail.com>
2017-04-07 21:49:26 +0200

    Optimized ParameterizedRendererTest

--- src/test/java/edu/hm/cs/swa/todo/Aufgabe01/ParameterizedRendererTest.java
+++ src/test/java/edu/hm/cs/swa/todo/Aufgabe01/ParameterizedRendererTest.java
@@ -17,19 +17,21 @@ public class ParameterizedRendererTest {
 
     private Renderer renderer;
     public static final int[] INTS = { 0, 5, 10 };
+    public static final int INTS_LEN = INTS.length;
 
     @Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][] {
-                { new SomeClass(INTS[0]),
-                        "Instance of edu.hm.cs.swa.todo.Aufgabe01.SomeClass:\n" + "foo (Type int): " + INTS[0]
-                                + "\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" },
-                { new SomeClass(INTS[1]),
-                        "Instance of edu.hm.cs.swa.todo.Aufgabe01.SomeClass:\n" + "foo (Type int): " + INTS[1]
-                                + "\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" },
-                { new SomeClass(INTS[2]), "Instance of edu.hm.cs.swa.todo.Aufgabe01.SomeClass:\n" + "foo (Type int): "
-                        + INTS[2]
-                        + "\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\n" } });
+
+        String expStrS = "Instance of edu.hm.cs.swa.todo.Aufgabe01.SomeClass:\n" + "foo (Type int): ";
+        String expStrM = "\narray (Type int[]) [1, 2, 3, ]\ndate (Type java.util.Date): Fri Jan 02 11:17:36 CET 1970\ngetDate (Returntype: class java.util.Date): Fri Jan 02 11:17:36 CET 1970\ngetFoo (Returntype: int): ";
+        String expStrL = "\ngetIntArray (Returntype: int[]) [1, 2, 3, ]\n";
+
+        Object[][] objArr = new Object[INTS_LEN][2];
+        for (int i = 0; i < INTS_LEN; i++) {
+            objArr[i][0] = new SomeClass(INTS[i]);
+            objArr[i][1] = expStrS + INTS[i] + expStrM + INTS[i] + expStrL;
+        }
+        return Arrays.asList(objArr);
     }
 
     @Parameter(0)
