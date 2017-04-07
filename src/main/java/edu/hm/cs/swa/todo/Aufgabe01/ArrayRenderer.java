d0cce6fc497c5ce1ee8556cb80bbb70c8149c340
Tobias Kroiss <tobi.kroiss@googlemail.com>
2017-04-07 19:42:53 +0200

    Adjustments (JavaDoc/Checkstyle)

--- src/main/java/edu/hm/cs/swa/todo/Aufgabe01/ArrayRenderer.java
+++ src/main/java/edu/hm/cs/swa/todo/Aufgabe01/ArrayRenderer.java
@@ -1,17 +1,24 @@
 package edu.hm.cs.swa.todo.Aufgabe01;
 
 /**
+ * This class is used to render an int[].
  * @author Dominik Grenz & Tobias Kroiss
  */
 public class ArrayRenderer {
 
-	public String render(int[] array) {
-		String result = "int[]) [";
-		for (int index : array) {
-			result += index + ", ";
-		}
-		result += "]\n";
-		return result;
-	}
+    /**
+     * This method creates a string-representation of an int[]
+     * (type & values).
+     * @param array int[] that has to be rendered
+     * @return string representation of the array's attributes
+     */
+    public String render(int[] array) {
+        String result = "int[]) [";
+        for (int index : array) {
+            result += index + ", ";
+        }
+        result += "]\n";
+        return result;
+    }
 
 }
